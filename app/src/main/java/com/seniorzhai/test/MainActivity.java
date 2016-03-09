f0288708bb8a06581164bd663db8407dc77afc00
zhaitao <370985116@qq.com>
2016-03-09 17:00:19 +0800

    界面

--- app/src/main/java/com/seniorzhai/test/MainActivity.java
+++ app/src/main/java/com/seniorzhai/test/MainActivity.java
@@ -1,13 +1,28 @@
 package com.seniorzhai.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), editText.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
